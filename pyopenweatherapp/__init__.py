d285e8bb7225e8a3de4112e5d97b6c0550ca8830
Kashi Revanna <kashi.revanna@verizonwireless.com>
2019-03-14 22:45:44 -0500

    initial setup

--- pyopenweatherapp/__init__.py
+++ pyopenweatherapp/__init__.py
@@ -7,8 +7,10 @@ API_KEY = '813ec3a13b14f61e6677112f831b877d'
 API_URL = 'http://api.openweathermap.org/data/2.5/weather?q={}&mode=json&units=metric&appid={}'
 
 def query_api(city):
+    """ Query API with city name """
     try:
         data = requests.get(API_URL.format(city, API_KEY)).json()
     except Exception as exc:
+        print(exc)
         data = None
     return data
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="pyopenweatherapp",
-    version="0.2.1",
+    version="0.1",
     include_package_data=True,
     install_requires=[
         'requests',
